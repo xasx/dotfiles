5fa9fa274e58947cc5bf85e9ac44a33bd4e0b064
Andreas Schöneck <as.maps@gmail.com>
2019-08-07 14:45:14 +0200

    [bobthefish] display process in title

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set -g theme_color_scheme terminal2-dark
 set -g theme_date_format "+%a,%d.%b %T"
 set -g theme_display_vi no
 set -g theme_display_user ssh
+set -g theme_title_display_process yes
 
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 thefuck --alias | source
