58106f06bf96395db808adde7172afa4a868e036
Andreas Schöneck <andreas.schoeneck@ionos.com>
2020-01-10 11:47:25 +0100

    Enlighten bobthefish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme terminal2-dark
+set -g theme_color_scheme terminal2-light-black
 set -g theme_date_format "+%a,%d.%b %T"
 set -g theme_display_vi no
 set -g theme_display_user ssh
